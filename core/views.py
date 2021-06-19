33bd6b6b346b16e3bb4944298ae3219360b2a9e8
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-19 21:08:20 +0530

    add : authorization and links in navbar

--- core/views.py
+++ core/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, "home.html")
 
 
+@login_required()
 def customer_page(request):
     return render(request, "home.html")
 
 
+@login_required()
 def courier_page(request):
     return render(request, "home.html")
