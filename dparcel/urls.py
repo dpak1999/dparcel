0f56b0d526ebc67c90577eb9dee0c0d9d8a50d8b
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-19 21:49:21 +0530

    add : sign up ui and and routes

--- dparcel/urls.py
+++ dparcel/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     # auth
     path("sign-in/", auth_views.LoginView.as_view(template_name="sign_in.html")),
     path("sign-out/", auth_views.LogoutView.as_view(next_page="/")),
+    path("sign-up", views.sign_up),
 
     # others
     path("customer/", views.customer_page),
