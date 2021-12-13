ca4297c4f94c4a97e4ed32e076ee92ec2307ea86
dpak1999 <deepakkumardash313@gmail.com>
2021-12-13 22:37:56 +0530

    add : social auth setting & views

--- dparcel/urls.py
+++ dparcel/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core import views
 
@@ -7,13 +7,16 @@ urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
 
+    # facebook auth url
+    path('', include('social_django.urls', namespace='social')),
+
     # home
     path("", views.home),
 
     # auth
     path("sign-in/", auth_views.LoginView.as_view(template_name="sign_in.html")),
     path("sign-out/", auth_views.LogoutView.as_view(next_page="/")),
-    path("sign-up", views.sign_up),
+    path("sign-up/", views.sign_up),
 
     # others
     path("customer/", views.customer_page),
