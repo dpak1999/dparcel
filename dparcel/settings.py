5f4c34a0ab74c6ac83cce0d257250eed3ed39040
dpak1999 <deepakkumardash313@gmail.com>
2021-12-16 00:40:59 +0530

    add : custome middleware for profiles

--- dparcel/settings.py
+++ dparcel/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middlewares.ProfileMiddleware',
 ]
 
 ROOT_URLCONF = 'dparcel.urls'
