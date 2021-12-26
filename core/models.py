d411b0b0b5be8c61f178c5d05d7d8bd5a6cd1fdb
dpak1999 <deepakkumardash313@gmail.com>
2021-12-27 00:11:55 +0530

    add : category & job model and migrations

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import uuid
 
 
 class Customer(models.Model):
@@ -13,3 +15,52 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+
+class Category(models.Model):
+    slug = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    SMALL_SIZE = "small"
+    MEDIUM_SIZE = "medium"
+    LARGE_SIZE = "large"
+    SIZES = (
+        (SMALL_SIZE, "Small"),
+        (MEDIUM_SIZE, "Medium"),
+        (LARGE_SIZE, "Large")
+    )
+    CREATING_STATUS = 'creating'
+    PROCESSING_STATUS = 'processing'
+    PICKING_STATUS = 'picking'
+    DELIVERING_STATUS = 'delivering'
+    COMPLETED_STATUS = 'completed'
+    CANCELLED_STATUS = 'cancelled'
+    STATUSES = (
+        (CREATING_STATUS, 'creating'),
+        (PROCESSING_STATUS, 'processing'),
+        (PICKING_STATUS, 'picking'),
+        (DELIVERING_STATUS, 'delivering'),
+        (COMPLETED_STATUS, 'completed'),
+        (CANCELLED_STATUS, 'cancelled'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    size = models.CharField(max_length=20, choices=SIZES, default=MEDIUM_SIZE)
+    quantity = models.IntegerField(default=1)
+    photo = models.ImageField(upload_to='job/photos')
+    status = models.CharField(
+        max_length=20, choices=STATUSES, default=CREATING_STATUS)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
