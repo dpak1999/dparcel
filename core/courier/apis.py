beaceaea8369fa1df744a282b24e3d557dee09df
deepak <deepakkumardash313@gmail.com>
2022-03-19 17:28:14 +0530

    upload photos in real time

--- core/courier/apis.py
+++ core/courier/apis.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from core.models import *
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 
 
 @csrf_exempt
@@ -33,11 +35,35 @@ def current_job_update_api(request, id):
         job.status = Job.DELIVERING_STATUS
         job.save()
 
+        try:
+            layer = get_channel_layer()
+            async_to_sync(layer.group_send)("job_"+str(job.id), {
+                'type': 'job_update',
+                'job': {
+                    'status': job.get_status_display(),
+                    'pickup_photo': job.pickup_photo.url,
+                }
+            })
+        except:
+            pass
+
     elif job.status == Job.DELIVERING_STATUS:
         job.delivery_photo = request.FILES["delivery_photo"]
         job.delivered_at = timezone.now()
         job.status = Job.COMPLETED_STATUS
         job.save()
+
+        try:
+            layer = get_channel_layer()
+            async_to_sync(layer.group_send)("job_"+str(job.id), {
+                'type': 'job_update',
+                'job': {
+                    'status': job.get_status_display(),
+                    'delivery_photo': job.delivery_photo.url,
+                }
+            })
+        except:
+            pass
 
     return JsonResponse({
         "success": True,
