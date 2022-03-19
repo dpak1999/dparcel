c684f5bbe757295df339b9cdd616fabf046caca1
deepak <deepakkumardash313@gmail.com>
2022-03-20 02:20:29 +0530

    update asgi

--- dparcel/asgi.py
+++ dparcel/asgi.py
@@ -1,14 +1,14 @@
 import os
-from dparcel.urls import websocket_urlpatterns
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dparcel.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dparcel.settings')
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from dparcel.urls import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dparcel.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dparcel.settings')
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
